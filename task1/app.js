4fd3dd17633526a075cdcb93c645f94b3f84432c
Samyah Ahmad <108360083+samyah5@users.noreply.github.com>
2022-08-02 14:09:34 +0300

    Add files via upload

---
+++ task1/app.js
@@ -0,0 +1,32 @@
+const texts = document.querySelector('.texts');
+window.SpeechRecognition =window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition =new window.SpeechRecognition();
+recognition.interimResults= true;
+
+let p = document.createElement('p');
+
+recognition.addEventListener('result',(e)=>{
+    const text=Array.from(e.results)
+     .map(result => result[0])
+     .map(result => result.transcript)
+     .join('');
+
+    p.innerText=text;
+    texts.appendChild(p);
+
+    if(e.results[0].isFinal){
+        p=document.createElement('p');
+       }
+
+    console.log(e);
+})
+
+recognition.addEventListener('end',()=>{
+    recognition.start();
+})
+recognition.start();
+
+
+const button1 =document.getElementById("button1");
+button1.addEventListener
